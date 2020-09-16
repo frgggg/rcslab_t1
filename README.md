cca4ea22e492fa7eac74e563405dddf55674e5dd
Fedor <zhar.feda@yandex.ru>
2020-09-16 12:00:04 +0300

    no docker - need mer, add readme and dev profile

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # rcslab_t1
+
+1) Install:
+1.1) need gradle 4.10 +
+1.2) tested on gradle-6.4.1
+1.3) build:
+# gradle bootJar
+
+2) Exec examples:
+2.1) X - port, Y - "jdbc:sqlite:" + path to data.sqlite + "/data.sqlite"
+java \
+    -Dspring.profiles.active=dev \
+    -Dserver.port=X \
+    -Dsqlite.path=Y \
+    -jar ./build/libs/InTest-1.1.jar
+
+2.2) X - port, Y - path to data.sqlite
+export RCSLAB_T1_PORT=X
+export RCSLAB_T1_DB_PATH=Y
+java \
+    -Dspring.profiles.active=dev \
+    -jar ./build/libs/InTest-1.1.jar
+    
+3) Use
+3.1) X - port, Y - path to data.sqlite
+3.2) Args: row, col. All must exist.
+3.2) Example
+http://localhost:X/?row=Y&col=Z
--- src/main/java/org/rcslab/repository/impl/TestRepositoryImpl.java
+++ src/main/java/org/rcslab/repository/impl/TestRepositoryImpl.java
@@ -1,5 +1,6 @@
 package org.rcslab.repository.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.rcslab.entity.RowColAnswer;
 import org.rcslab.entity.RowColRequest;
 import org.rcslab.exception.ServiceException;
@@ -13,15 +14,18 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Component
 public class TestRepositoryImpl implements TestRepository {
+
+    private static final String ROW_COL_REQUEST = "SELECT %s as row, %s as col, COUNT(*) as val FROM source_data GROUP BY %s, %s";
+
     @Value("${sqlite.path}")
     private String connectionString;
-    private static final String ROW_COL_REQUEST = "SELECT %s as row, %s as col, COUNT(*) as val FROM source_data GROUP BY %s, %s";
     private Connection connection = null;
 
     @Override
-    public List<RowColAnswer> execRowColRequest(RowColRequest request) {
+    public synchronized List<RowColAnswer> execRowColRequest(RowColRequest request) {
         if(connection == null) {
             throw new ServiceException("no connection with db");
         }
@@ -64,7 +68,7 @@ public class TestRepositoryImpl implements TestRepository {
 
     @PostConstruct
     public void postConstruct() {
-        System.out.println("connectionString = " + connectionString);
+        log.info("connectionString = " + connectionString);
         if(connection == null) {
             try {
                 connection = DriverManager.getConnection(connectionString);
